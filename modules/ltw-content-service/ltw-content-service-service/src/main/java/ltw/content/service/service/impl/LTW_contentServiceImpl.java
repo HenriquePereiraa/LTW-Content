f8ca50dcaa51929c3d94deddc2ca77237ee9a98f
Henrique Pereira <henrique.pereira@liferay.com>
2024-08-13 17:48:35 -0300

    feat: Add check permission to LTW_contentServiceImpl

--- modules/ltw-content-service/ltw-content-service-service/src/main/java/ltw/content/service/service/impl/LTW_contentServiceImpl.java
+++ modules/ltw-content-service/ltw-content-service-service/src/main/java/ltw/content/service/service/impl/LTW_contentServiceImpl.java
@@ -7,6 +7,9 @@ package ltw.content.service.service.impl;
 
 import com.liferay.portal.aop.AopService;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import ltw.content.service.model.LTW_content;
 import ltw.content.service.service.LTW_contentLocalService;
 import ltw.content.service.service.base.LTW_contentServiceBaseImpl;
@@ -26,10 +29,18 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class LTW_contentServiceImpl extends LTW_contentServiceBaseImpl {
 
-//	public LTW_content addLTW_content(String motorcycleName, String motorcycleManufacturing, int motorcycleYear) {
-//
-//	}
+	public LTW_content addLTW_content(String motorcycleName, String motorcycleManufacturing, int motorcycleYear) throws PortalException {
+		_modelResourcePermission.check(
+				getPermissionChecker(), null, ActionKeys.ADD_ENTRY
+		);
+
+		return _ltwContentLocalService.addLTW_content(motorcycleName, motorcycleManufacturing, motorcycleYear);
+	}
 
 	@Reference
 	LTW_contentLocalService _ltwContentLocalService;
+
+	ModelResourcePermission<LTW_content>
+		_modelResourcePermission;
+
 }
